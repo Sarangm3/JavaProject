6b2b03a26d34c70e06ad93545134a30ae8c260ca
Sarangm3 <sarangm3712@gmail.com>
2023-02-21 09:44:08 +0530

    Pratical_01_Database and Practical_01_Server done Practical_02 just start

---
+++ src/Practical_01_DataBase.java
@@ -0,0 +1,155 @@
+import java.sql.*;
+
+class Data{
+    int paperCode
+            ;
+    String subName;
+    String paperDetail;
+    String url;
+    Data(int paperCode
+            ,String subName,String paperDetail,String url){
+        this.paperCode
+                = paperCode
+        ;
+        this.subName = subName; this.paperDetail = paperDetail;
+        this.url = url;
+    }
+}
+public class Practical_01_DataBase {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/db";
+    static final String USER = "root";
+    static final String PASS = "SArang3@";
+    public static Connection getConnection() throws Exception {
+        Class.forName(JDBC_DRIVER);
+        String url = "jdbc:mysql://localhost:3306/mydatabase"; // Replace with your own database URL
+        String user = "username"; // Replace with your own username
+        String password = "password"; // Replace with your own password
+        return DriverManager.getConnection(url, user, password);
+    }
+    static void createTable(Statement stmt){
+        try{
+            String sql = "CREATE TABLE Papers " +
+                    "(paperCode" +
+                    " INTEGER not NULL, " +
+                    " subName VARCHAR(255), " +
+                    " paperDetail VARCHAR(255), " +
+                    " url VARCHAR(255), " +
+                    " PRIMARY KEY (paperCode" +
+                    "))";
+            stmt.executeUpdate(sql);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    static void insertPaperDetail(Statement stmt,Data newData){
+        try {
+            String sql = "INSERT INTO Papers (paperCode" +
+                    ", subName, paperDetail, url) " +
+                    "VALUES (" + newData.paperCode
+                    +
+                    ", '" + newData.subName +
+                    "', '" + newData.paperDetail +
+                    "', '" + newData.url + "')";
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    static ResultSet searchData(Statement stmt){
+        try{
+            String sql = "SELECT paperCode" +
+                    ", subName, paperDetail, url FROM Papers";
+            ResultSet rs = stmt.executeQuery(sql);
+            return rs;
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+    static void updatePaperDetail(Statement stmt, Data updatedData){
+        try {
+            String sql = "UPDATE Papers " +
+                    "SET subName = '" + updatedData.subName + "', " +
+                    "paperDetail = '" + updatedData.paperDetail + "', " +
+                    "url = '" + updatedData.url + "' " +
+                    "WHERE paperCode" +
+                    " = " + updatedData.paperCode
+                    ;
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    static void deletePaperDetail(Statement stmt, int paperCode){
+        try {
+            String sql = "DELETE FROM Papers WHERE paperCode" +
+                    " = " + paperCode
+                    ;
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    static ResultSet searchData(Statement stmt, String searchString){
+        try{
+            String sql = "SELECT subID, subName, paperDetail, url FROM Papers " +
+                    "WHERE subName LIKE '%" + searchString + "%' OR paperDetail LIKE '%" + searchString + "%'";
+            ResultSet rs = stmt.executeQuery(sql);
+            return rs;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    static String getSearchResult(ResultSet rs) throws Exception {
+        StringBuilder searchResult = new StringBuilder();
+        while (rs.next()) {
+            searchResult.append(rs.getString("subName")).append(" | ");
+            searchResult.append(rs.getString("paperDetail")).append(" | ");
+            searchResult.append(rs.getString("url")).append("\n");
+        }
+        return searchResult.toString();
+    }
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+
+            conn = getConnection();
+            stmt = conn.createStatement();
+
+            // Creating table
+//            createTable(stmt);
+
+            //Insert data
+//            insertPaperDetail(stmt,new Data(3160714,"DATA MINING","2019_paper","http://example.com/paper"));
+
+            // Updating paper detail
+//            updatePaperDetail(stmt, new Data(3160714,"DATA MINING","2020_paper","http://example.com/paper"));
+
+            // Deleting paper detail
+//            deletePaperDetail(stmt, 3160714);
+
+            //search from database
+            ResultSet rs = searchData(stmt);
+            // Displaying data
+            System.out.println(getSearchResult(rs));
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Practical_01_Server.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.net.*;
+import java.sql.*;
+
+public class Practical_01_Server extends Practical_01_DataBase {
+
+    private static final int PORT = 12345; // the port number the server listens on
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server listening on port " + PORT);
+
+            // listen for incoming client connections
+            while (true) {
+                // accept incoming client connection
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket.getInetAddress());
+
+                // create new thread to handle search request
+                Thread thread = new Thread(() -> {
+                    try {
+                        // handle search request
+                        Connection connection = getConnection();
+                        ResultSet resultSet = searchData(connection.createStatement(), "java");
+                        String searchResult = getSearchResult(resultSet);
+                        clientSocket.getOutputStream().write(searchResult.getBytes());
+                        System.out.println("Search result sent to client.");
+                    } catch (Exception e) {
+                        System.out.println("Error handling search request: " + e.getMessage());
+                    } finally {
+                        // close client connection
+                        try {
+                            clientSocket.close();
+                            System.out.println("Client disconnected.");
+                        } catch (IOException e) {
+                            System.out.println("Error closing client connection: " + e.getMessage());
+                        }
+                    }
+                });
+                thread.start();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
